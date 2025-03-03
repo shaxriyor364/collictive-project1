973c5982ffaa19c2367337b17e0be0478859965f
unknown <your-email@example.com>
2025-03-03 07:01:43 +0500

    shaxriyor

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//hamburger
+let navLink = document.getElementById("nav-menu");
+let navMenu = document.getElementById("hamburger");
+navMenu.addEventListener("click", () => {
+  navLink.classList.toggle("active");
+});
+
+//dark-mode
+let modeBtn = document.getElementById("mode-btn");
+
+modeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+});
+
+
+const loading = document.getElementById("loading");
+
+const loadingDuration = 5000; // 1.8s
+
+setTimeout(() => {
+  loading.classList.add("loading-none");
+}, loadingDuration);
